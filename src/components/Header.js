6e8b5270654325efeccea57ab7e3ba4fdb2f4648
Zenia <zeniavilla@hotmail.com>
2017-10-30 16:57:40 -0700

    Add day names row to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,15 @@ import React from 'react';
 const Header = props => {
   const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const dayAbbrev = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
   const { month, year } = props.selectedDate;
+  const displayDays = () => {
+    return dayNames.map(day => 
+      <div className="col">
+        <h4>{day}</h4>
+      </div>
+    )};
+
   return (
     <div>
 
@@ -12,6 +20,9 @@ const Header = props => {
         {monthNames[month]} {year}
         <i className="fa fa-angle-right" onClick={props.nextMonth} aria-hidden="true"></i>
       </h1>
+      <div className="row">
+        {displayDays()}
+      </div>
     </div>
   )
 }
