58ffb91ef46afff67e6be895e6c15f8d4168e0b9
Zenia <zeniavilla@hotmail.com>
2017-10-30 16:37:53 -0700

    Add arrow icons with click action to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 const Header = props => {
   const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];  
+  const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const { month, year } = props.selectedDate;
   return (
     <div>
-      <h1>{monthNames[month]} {year}</h1>
+
+      <h1>
+        <i className="fa fa-angle-left" onClick={props.prevMonth} aria-hidden="true"></i>
+        {monthNames[month]} {year}
+        <i className="fa fa-angle-right" onClick={props.nextMonth} aria-hidden="true"></i>
+      </h1>
     </div>
   )
 }
