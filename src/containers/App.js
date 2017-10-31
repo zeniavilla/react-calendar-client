f0100ffc2a1ed8673fea35ac1056712e2280c2c0
Zenia <zeniavilla@hotmail.com>
2017-10-31 14:03:56 -0700

    Add numDaysInMonth function to determine dates

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 // Components
 import Header from '../components/Header';
+import Dates from '../components/Dates';
 import changeDate from '../actions/DateActions';
 
 class App extends Component {
@@ -13,6 +14,20 @@ class App extends Component {
     } else {
       this.props.changeDate({month: currDate.month - 1});
     }
+  }
+
+  // returns array of month dates
+  numDaysInMonth = (year, month) => {
+    let numDays = new Date(year, month + 1, 0).getDate();
+    let datesArray = Array.apply(null, {length: numDays}).map(Number.call, Number);
+    for (let i = 0; i < this.firstDayOfMonth(year, month + 1); i++) {
+      datesArray.unshift('');
+    }
+    return datesArray;
+  }
+
+  firstDayOfMonth = (year, month) => {
+    return new Date(year, month - 1, 1).getDay();
   }
 
   nextMonth = event => {
@@ -28,6 +43,12 @@ class App extends Component {
     return (
       <div className="calendar">
         <Header selectedDate={this.props.selectedDate} prevMonth={this.prevMonth} nextMonth={this.nextMonth} />
+        {
+          this.numDaysInMonth(this.props.selectedDate.year, this.props.selectedDate.month).map(
+            date => <Dates key={date} date={date + 1} />
+          )
+        }
+        
       </div>
     );
   }
