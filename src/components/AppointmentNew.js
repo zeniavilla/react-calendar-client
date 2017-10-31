8449f0c9dffd18dd7613419045277288eba00711
Zenia <zeniavilla@hotmail.com>
2017-10-31 16:50:23 -0700

    Add handleTitleChange to appointment form

--- src/components/AppointmentNew.js
+++ src/components/AppointmentNew.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AppointmentNew = props => {
   return (
-    <form className="appt-form">
+    <form className="appt-form" onChange={props.handleTitleChange}>
       <input type="textarea" />
       <button className="btn-primary" onClick={props.handleNewAppt}>Add</button>
     </form>
