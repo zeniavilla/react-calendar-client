3c34dbf74e89e6c06f7c80860dbbcc9a64c9ea73
Zenia <zeniavilla@hotmail.com>
2017-10-31 16:51:10 -0700

    Add updateTitleChange to update form title in state

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,7 +4,12 @@ import { connect } from 'react-redux';
 // Components
 import Header from '../components/Header';
 import Dates from '../components/Dates';
+import AppointmentNew from '../components/AppointmentNew';
+
+// Actions
 import changeDate from '../actions/DateActions';
+import createAppointment from '../actions/AppointmentActions';
+import updateTitle from '../actions/AppointmentFormActions';
 
 class App extends Component {
   prevMonth = () => {
@@ -13,6 +18,15 @@ class App extends Component {
       this.props.changeDate({ month: 11, year: currDate.year - 1, day: '' });
     } else {
       this.props.changeDate({ month: currDate.month - 1, day: '' });
+    }
+  }
+  
+  nextMonth = () => {
+    let currDate = this.props.selectedDate;
+    if (currDate.month === 11) {
+      this.props.changeDate({ month: 0, year: currDate.year + 1, day: '' });
+    } else {
+      this.props.changeDate({ month: currDate.month + 1, day: '' });
     }
   }
 
@@ -30,21 +44,27 @@ class App extends Component {
     return new Date(year, month - 1, 1).getDay();
   }
 
-  nextMonth = () => {
-    let currDate = this.props.selectedDate;
-    if (currDate.month === 11) {
-      this.props.changeDate({ month: 0, year: currDate.year + 1, day: '' });
-    } else {
-      this.props.changeDate({ month: currDate.month + 1, day: '' });
-    }
-  }
-
   handleOnClick = event => {
     if (this.props.selectedDate.day !== '') {
       document.getElementById(`day-${this.props.selectedDate.day}`).classList.remove("selected")
     }
     document.getElementById(event.target.id).className += " selected";
     this.props.changeDate({ day: parseInt(event.target.id.split('-')[1]) });
+  }
+
+  createAppt = event => {
+    event.preventDefault();
+    console.log(event.target)
+    // this.props.createAppointment(this.props.selectedDate);
+  }
+
+  updateTitleChange = event => {
+    const currentTitle = Object.assign({},
+      this.props.appointmentForm, {
+        title: event.target.value
+    });
+
+    this.props.updateTitle(currentTitle);
   }
 
   // Reset 'selected' date class when switching to a new month
@@ -56,23 +76,28 @@ class App extends Component {
 
   render() {
     return (
-      <div className="calendar">
-        <Header selectedDate={this.props.selectedDate} prevMonth={this.prevMonth} nextMonth={this.nextMonth} />
-        <div className="dates-rows">
-          {
-            this.numDaysInMonth(this.props.selectedDate.year, this.props.selectedDate.month).map(
-              date => <Dates key={date} date={date + 1} handleOnClick={this.handleOnClick} />
-            )
-          }
+      <div>
+        <div className="calendar">
+          <Header selectedDate={this.props.selectedDate} prevMonth={this.prevMonth} nextMonth={this.nextMonth} />
+          <div className="dates-rows">
+            {
+              this.numDaysInMonth(this.props.selectedDate.year, this.props.selectedDate.month).map(
+                date => <Dates key={date} date={date + 1} handleOnClick={this.handleOnClick} />
+              )
+            }
+          </div>
         </div>
-        
+        {this.props.selectedDate.day !== '' ? <AppointmentNew handleNewAppt={this.createAppt} handleTitleChange={this.updateTitleChange} /> : 'Select a date to schedule an appointment.'}
       </div>
     );
   }
 }
 
 const mapStateToProps = state => {
-  return ({ selectedDate: state.selectedDate })
+  return ({ 
+    selectedDate: state.selectedDate,
+    appointmentForm: state.appointmentForm
+  })
 }
 
-export default connect(mapStateToProps, { changeDate })(App);
+export default connect(mapStateToProps, { changeDate, createAppointment, updateTitle })(App);
