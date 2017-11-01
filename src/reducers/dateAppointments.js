327ab8dc7175082e9bbec929c8695a8d636d620d
Zenia <zeniavilla@hotmail.com>
2017-11-01 14:20:04 -0700

    Filter state to return new array without specified id in REMOVE_APPOINTMENT

--- src/reducers/dateAppointments.js
+++ src/reducers/dateAppointments.js
@@ -4,6 +4,8 @@ export const dateAppointments = (state = [], action) => {
       return action.appointments;
     case 'ADD_APPOINTMENT':
       return [...state, action.appointment];
+    case 'REMOVE_APPOINTMENT':
+      return state.filter(obj => obj.id !== parseInt(action.id));
     default:
       return state;
   }
