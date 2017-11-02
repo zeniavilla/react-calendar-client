9313d52fd7fa31ba06ee15d7a2e7afa281ae19b1
Zenia <zeniavilla@hotmail.com>
2017-11-02 10:26:41 -0700

    Change 'add' text to plus fa icon in AppointmentNew

--- src/components/AppointmentNew.js
+++ src/components/AppointmentNew.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+// styles
+import '../styles/components/appointmentForm.css';
+
 const AppointmentNew = props => {
   return (
     <form className="appt-form" onChange={props.handleTitleChange}>
-      <input type="textarea" value={props.appointmentForm.title} />
-      <button className="btn-primary" onClick={props.handleNewAppt}>Add</button>
+      <input type="textarea" value={props.appointmentForm.title} placeholder="Title" />
+      <button className="btn-primary" onClick={props.handleNewAppt}><i class="fa fa-plus" aria-hidden="true"></i></button>
     </form>
   )
 }
