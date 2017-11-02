09171682f693a43f58cc2eec2cdc83ce31903232
Zenia <zeniavilla@hotmail.com>
2017-11-02 11:03:34 -0700

    Change <li> to <div> in AppointmentListItem and add row classes

--- src/components/AppointmentListItem.js
+++ src/components/AppointmentListItem.js
@@ -3,10 +3,12 @@ import React from 'react';
 const AppointmentListItem = props => {
   const { id, title } = props.appointment;
   return (
-    <li>
+    <div className="col-cal light-row">
       <span className="appointment-details">{title}</span>
-      <button className="btn-delete" onClick={props.deleteAppointment} id={id}>Delete</button>
-    </li>
+      <button className="btn-delete" onClick={props.deleteAppointment}>
+        <i className="fa fa-times" aria-hidden="true" id={id}></i>
+      </button>
+    </div>
   )
 }
 
